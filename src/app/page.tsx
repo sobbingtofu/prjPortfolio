3f94720be81549180aea21fa9e19778b1acf40fb
sobbingtofu <kukubasak@naver.com>
2024-07-19 15:24:14 +0900

    script: swiper 패키지 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Page from "@/components/common/Page";
 export default function Home() {
   return (
     <Page>
-      <h1 className="text-3xl">프론트엔드 개발자 윤기준 포트폴리오</h1>
+      <h1 className="text-4xl font-bold">
+        프론트엔드 개발자 윤기준 포트폴리오
+      </h1>
+      <h3 className="text-2xl font-bold mt-16">기술 스택</h3>
     </Page>
   );
 }
