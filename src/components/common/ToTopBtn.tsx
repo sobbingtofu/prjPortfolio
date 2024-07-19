023d1f6f9ec1a538a1d41a34ac2702d1ce774d46
sobbingtofu <kukubasak@naver.com>
2024-07-20 02:12:21 +0900

    style: 맨위로가는버튼 모바일에서도 보이도록 변경

--- src/components/common/ToTopBtn.tsx
+++ src/components/common/ToTopBtn.tsx
@@ -3,7 +3,7 @@
 function ToTopBtn() {
   return (
     <button
-      className="fixed bottom-10 right-10 hidden h-[50px] w-[50px] rounded-[100%] bg-gray-200 shadow-md transition-transform duration-300 hover:-translate-y-2 md:block"
+      className="fixed bottom-10 right-10 h-[50px] w-[50px] rounded-[100%] bg-gray-200 shadow-md transition-transform duration-300 hover:-translate-y-2"
       onClick={() => {
         window.scrollTo({
           top: 0,
